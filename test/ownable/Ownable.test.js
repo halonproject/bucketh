875c6967ea7e1ff0c3e074a2c9f6fa1ff9f988c4
Chris Purta <cpurta@kochava.com>
2018-05-04 09:40:03 -0700

    Update Ownable contracts
    Update ownable contract to allow for renouncement of ownership. Also
    added testing files for the contract so that we can ensure they are
    working as expected. Added assertThrow testing helpers for ownable
    testing files.

--- test/ownable/Ownable.test.js
+++ test/ownable/Ownable.test.js
@@ -1,21 +1,21 @@
 const { assertRevert } = require('../helpers/assertThrow');
 
-var Ownable = artifacts.require('Ownable');
+const Ownable = artifacts.require('Ownable');
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
-contract('Ownable', function (accounts) {
+contract('Ownable', accounts => {
   let ownable;
 
-  beforeEach(async function () {
+  beforeEach(async () => {
     ownable = await Ownable.new();
   });
 
-  it('should have an owner', async function () {
+  it('should have an owner', async () => {
     let owner = await ownable.owner();
     assert.isTrue(owner !== 0);
   });
 
-  it('changes owner after transfer', async function () {
+  it('changes owner after transfer', async () => {
     let other = accounts[1];
     await ownable.transferOwnership(other);
     let owner = await ownable.owner();
@@ -23,29 +23,35 @@ contract('Ownable', function (accounts) {
     assert.isTrue(owner === other);
   });
 
-  it('should prevent non-owners from transfering', async function () {
+  it('should prevent non-owners from transfering', async () => {
     const other = accounts[2];
     const owner = await ownable.owner.call();
     assert.isTrue(owner !== other);
-    await assertRevert(ownable.transferOwnership(other, { from: other }));
+    return assertRevert( async () => {
+        await ownable.transferOwnership(other, { from: other })
+    });
   });
 
-  it('should guard ownership against stuck state', async function () {
+  it('should guard ownership against stuck state', async () => {
     let originalOwner = await ownable.owner();
-    await assertRevert(ownable.transferOwnership(null, { from: originalOwner }));
+    return assertRevert( async () => {
+        await ownable.transferOwnership(null, { from: originalOwner })
+    });
   });
 
-  it('loses owner after renouncement', async function () {
+  it('loses owner after renouncement', async () => {
     await ownable.renounceOwnership();
     let owner = await ownable.owner();
 
     assert.isTrue(owner === ZERO_ADDRESS);
   });
 
-  it('should prevent non-owners from renouncement', async function () {
+  it('should prevent non-owners from renouncement', async () => {
     const other = accounts[2];
     const owner = await ownable.owner.call();
     assert.isTrue(owner !== other);
-    await assertRevert(ownable.renounceOwnership({ from: other }));
+    return assertRevert( async () => {
+        await ownable.renounceOwnership({ from: other })
+    });
   });
 });
