49941b4aa14d087c4f912531ebad483f8bc7f7fc
Chris Purta <cpurta@kochava.com>
2018-07-05 11:27:26 -0700

    Match truffle network to any network_id

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,8 @@
 module.exports = {
     networks: {
       rpc: {
-        network_id: 15,
-        host: 'localhost',
+        network_id: "*",
+        host: "localhost",
         port: 8545,
         gas: 6.9e6,
       },
